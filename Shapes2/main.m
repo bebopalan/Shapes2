a6af7caff38e7718587a7d4f74fbf2d262502e23
Jacob Luetzow <jluetzow@macbook-pro.gateway.2wire.net>
2014-02-20 22:51:38 -0600

    Initial Commit

---
+++ Shapes2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Shapes2
+//
+//  Created by Jacob Luetzow on 2/20/14.
+//  Copyright (c) 2014 Jacob Luetzow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
